65620e2a9750988f18552e15b89e00a11eb7a3f0
Ansel Rognlie <ansel.rognlie@gmail.com>
2023-10-22 13:19:23 -0700

    Fixed broken tests after signature change

--- tests/adder/test_fixed_width_math.py
+++ tests/adder/test_fixed_width_math.py
@@ -3,32 +3,44 @@ import cflang.cfsm.fixed_width_math as FixedWidthAdder
 def test_add_with_overflow():
     n1 = 0x7f
     n2 = 0x01
-    r, v = FixedWidthAdder.add(1, n1, n2)
+    r, flags = FixedWidthAdder.add(1, n1, n2)
 
     assert r == 0x80
-    assert v is True
+    assert flags.overflow is True
+    assert flags.zero is False
+    assert flags.carry is False
+    assert flags.negative is True
 
 def test_add_wrap_no_overflow():
     n1 = 0xff
     n2 = 0x01
-    r, v = FixedWidthAdder.add(1, n1, n2)
+    r, flags = FixedWidthAdder.add(1, n1, n2)
 
     assert r == 0x00
-    assert v is False
+    assert flags.overflow is False
+    assert flags.zero is True
+    assert flags.carry is True
+    assert flags.negative is False
 
 def test_sub_with_overflow():
     n1 = 0x80
     n2 = 0x01
-    r, v = FixedWidthAdder.sub(1, n1, n2)
+    r, flags = FixedWidthAdder.sub(1, n1, n2)
 
     assert r == 0x7f
-    assert v is True
+    assert flags.overflow is True
+    assert flags.zero is False
+    assert flags.carry is False
+    assert flags.negative is False
 
 def test_sub_wrap_no_overflow():
     n1 = 0x00
     n2 = 0x01
-    r, v = FixedWidthAdder.sub(1, n1, n2)
+    r, flags = FixedWidthAdder.sub(1, n1, n2)
 
     assert r == 0xff
-    assert v is False
+    assert flags.overflow is False
+    assert flags.zero is False
+    assert flags.carry is True
+    assert flags.negative is True
 
